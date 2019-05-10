c02ebb2fc717da58ab0b1df99a040e71c402e90f
Denys Smirnov <denis.smirnov.91@gmail.com>
2019-05-10 22:46:34 +0300

    refactor proxy into a library

--- proxy.go
+++ proxy.go
@@ -18,31 +18,25 @@
 	of the GNU General Public License.
 */
 
-// TLS Proxy 0.0.1.7
+// TLS Proxy 0.0.2.0
 
 package main
 
 import (
-	"bytes"
-	"crypto/tls"
 	"flag"
-	"io"
 	"log"
-	"net"
-	"net/http"
 	_ "net/http/pprof"
-	"os"
 	"strings"
-	"sync"
 	"time"
 
-	"github.com/Verlihub/tls-proxy/metrics"
+	"github.com/Verlihub/tls-proxy/dcproxy"
 )
 
 var (
 	fHost    = flag.String("host", ":411", "Comma-separated list of hosts to listen on")
 	fWait    = flag.Duration("wait", 650*time.Millisecond, "Time to wait to detect the protocol")
 	fHub     = flag.String("hub", "127.0.0.1:411", "Hub address to connect to")
+	fHubNet  = flag.String("net", "tcp4", "Hub network (tcp4, tcp6, tcp, unix)")
 	fIP      = flag.Bool("ip", true, "Send client IP")
 	fLog     = flag.Bool("log", false, "Enable connection logging")
 	fCert    = flag.String("cert", "hub.cert", "TLS .cert file")
@@ -54,304 +48,35 @@ var (
 
 func main() {
 	flag.Parse()
-	os.Setenv("GODEBUG", os.Getenv("GODEBUG")+",tls13=1")
 
 	if err := run(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-var tlsConfig *tls.Config
-
 func run() error {
-	if *fCert != "" && *fKey != "" {
-		log.Println("using certs:", *fCert, *fKey)
-		var err error
-		tlsConfig = &tls.Config{NextProtos: []string{"nmdc"}}
-		tlsConfig.Certificates = make([]tls.Certificate, 1)
-		tlsConfig.Certificates[0], err = tls.LoadX509KeyPair(*fCert, *fKey)
-
-		if err != nil {
-			return err
-		}
-
-	} else {
-		log.Println("no certs; TLS disabled")
+	p, err := dcproxy.New(dcproxy.Config{
+		HubAddr:    *fHub,
+		HubNetwork: *fHubNet,
+		Hosts:      strings.Split(*fHost, ","),
+		Cert:       *fCert,
+		Key:        *fKey,
+		PProf:      *fPProf,
+		Metrics:    *fMetrics,
+		LogErrors:  *fLog,
+		Wait:       *fWait,
+		Buffer:     *fBuf,
+		NoSendIP:   !*fIP,
+	})
+	if err != nil {
+		return err
 	}
-
-	if *fPProf != "" {
-		log.Println("enabling profiler on", *fPProf)
-
-		go func() {
-			if err := http.ListenAndServe(*fPProf, nil); err != nil {
-				log.Println("cannot enable profiler:", err)
-			}
-		}()
+	err = p.Run()
+	if err != nil {
+		return err
 	}
+	defer p.Close()
 
-	if *fMetrics != "" {
-		log.Println("serving metrics on", *fMetrics)
-
-		go func() {
-			if err := metrics.ListenAndServe(*fMetrics); err != nil {
-				log.Println("cannot serve metrics:", err)
-			}
-		}()
-	}
-
-	hosts := strings.Split(*fHost, ",")
-	var lis []net.Listener
-
-	defer func() {
-		for _, l := range lis {
-			_ = l.Close()
-		}
-	}()
-
-	for _, host := range hosts {
-		l, err := net.Listen("tcp4", host)
-
-		if err != nil {
-			return err
-		}
-
-		lis = append(lis, l)
-	}
-
-	var wg sync.WaitGroup
-
-	for i, l := range lis {
-		wg.Add(1)
-		l := l
-		log.Println("proxying", hosts[i], "to", *fHub)
-
-		go func() {
-			defer wg.Done()
-			acceptOn(l)
-		}()
-	}
-
-	wg.Wait()
+	p.Wait()
 	return nil
 }
-
-func acceptOn(l net.Listener) {
-	for {
-		c, err := l.Accept()
-
-		if err != nil {
-			if *fLog {
-				log.Println(err)
-			}
-
-			metrics.ConnError.Add(1)
-			continue
-		}
-
-		metrics.ConnAccepted.Add(1)
-
-		go func() {
-			err := serve(c)
-
-			if err != nil && err != io.EOF {
-				metrics.ConnError.Add(1)
-
-				if *fLog {
-					log.Println(c.RemoteAddr(), err)
-				}
-			}
-		}()
-	}
-}
-
-type timeoutErr interface {
-	Timeout() bool
-}
-
-func serve(c net.Conn) error {
-	metrics.ConnOpen.Add(1)
-
-	defer func() {
-		_ = c.Close()
-		metrics.ConnOpen.Add(-1)
-	}()
-
-	buf := make([]byte, 1024)
-	i := copy(buf, "$MyIP ")
-	i += copy(buf[i:], c.RemoteAddr().(*net.TCPAddr).IP.String())
-	i += copy(buf[i:], " 0.0|")
-
-	if tlsConfig == nil || *fWait <= 0 { // no auto detection
-		metrics.ConnInsecure.Add(1)
-		metrics.ConnOpenInsecure.Add(1)
-		defer metrics.ConnOpenInsecure.Add(-1)
-		return writeAndStream(buf[:i], c, i)
-	}
-
-	err := c.SetReadDeadline(time.Now().Add(*fWait))
-
-	if err != nil {
-		return err
-	}
-
-	start := time.Now()
-	n, err := c.Read(buf[i:])
-	_ = c.SetReadDeadline(time.Time{})
-
-	if e, ok := err.(timeoutErr); ok && e.Timeout() { // has to be plain nmdc
-		metrics.ConnInsecure.Add(1)
-		metrics.ConnOpenInsecure.Add(1)
-		defer metrics.ConnOpenInsecure.Add(-1)
-		return writeAndStream(buf[:i], c, i)
-	}
-
-	if err != nil {
-		return err
-	}
-
-	buf = buf[:i+n]
-
-	if n >= 2 && buf[i] == 0x16 && buf[i+1] == 0x03 {
-		tlsBuf := buf[i:]
-		c = &multiReadConn{r: io.MultiReader(bytes.NewReader(tlsBuf), c), Conn: c}
-		tc := tls.Server(c, tlsConfig) // tls handshake
-		defer tc.Close()
-		err = tc.Handshake()
-
-		if err != nil {
-			return err
-		}
-
-		buf[i-4] = '1' // set version
-		state := tc.ConnectionState()
-
-		switch state.Version {
-		case tls.VersionTLS13:
-			buf[i-2] = '3'
-		case tls.VersionTLS12:
-			buf[i-2] = '2'
-		case tls.VersionTLS11:
-			buf[i-2] = '1'
-			//default:
-			//buf[i-2] = '0'
-		}
-
-		buf = buf[:i]
-		dt := time.Since(start).Seconds()
-		metrics.ConnTLS.Add(1)
-		metrics.ConnOpenTLS.Add(1)
-		defer metrics.ConnOpenTLS.Add(-1)
-		metrics.ConnTLSHandshake.Observe(dt)
-		return writeAndStream(buf, tc, i)
-	}
-
-	metrics.ConnInsecure.Add(1)
-	metrics.ConnOpenInsecure.Add(1)
-	defer metrics.ConnOpenInsecure.Add(-1)
-	return writeAndStream(buf, c, i)
-}
-
-type multiReadConn struct {
-	r io.Reader
-	net.Conn
-}
-
-func (c *multiReadConn) Read(b []byte) (int, error) {
-	return c.r.Read(b)
-}
-
-func dialHub() (net.Conn, error) {
-	return net.Dial("tcp4", *fHub)
-}
-
-func writeAndStream(p []byte, c io.ReadWriteCloser, i int) error {
-	h, err := dialHub()
-
-	if err != nil {
-		return err
-	}
-
-	defer h.Close()
-
-	if !*fIP {
-		p = p[i:]
-	}
-
-	if len(p) > 0 {
-		_, err = h.Write(p)
-
-		if err != nil {
-			return err
-		}
-	}
-
-	return stream(c, h)
-}
-
-func stream(c, h io.ReadWriteCloser) error {
-	closeBoth := func() {
-		_ = c.Close()
-		_ = h.Close()
-	}
-
-	defer closeBoth()
-
-	go func() {
-		defer closeBoth()
-		_, _ = copyBuffer(h, c, metrics.ConnRx)
-	}()
-
-	_, _ = copyBuffer(c, h, metrics.ConnTx)
-	return nil
-}
-
-// copyBuffer was copied from io package and modified to add instrumentation
-
-func copyBuffer(dst io.Writer, src io.Reader, cnt metrics.Counter) (written int64, err error) {
-	size := *fBuf * 1024
-
-	if l, ok := src.(*io.LimitedReader); ok && int64(size) > l.N {
-		if l.N < 1 {
-			size = 1
-		} else {
-			size = int(l.N)
-		}
-	}
-
-	buf := make([]byte, size)
-
-	for {
-		nr, er := src.Read(buf)
-
-		if nr > 0 {
-			nw, ew := dst.Write(buf[0:nr])
-			cnt.Add(float64(nw))
-
-			if nw > 0 {
-				written += int64(nw)
-			}
-
-			if ew != nil {
-				err = ew
-				break
-			}
-
-			if nr != nw {
-				err = io.ErrShortWrite
-				break
-			}
-		}
-
-		if er != nil {
-			if er != io.EOF {
-				err = er
-			}
-
-			break
-		}
-	}
-
-	return written, err
-}
-
-// end of file
